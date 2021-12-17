d53f658d0697b1551af765076c36d2c5c428c412
Luka Lortkipanidze <l.lortkifanidze@noxtton.com>
2021-12-17 19:17:07 +0400

    checking templates

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("starter.html")
 
+
+@app.route("/loading")
+def loading():
+    return render_template("spinner.html")
 
 if "__main__" == __name__:
     app.run()
