74d499e48a411882633eb436be2c414c26ab931e
Luka Lortkipanidze <l.lortkifanidze@noxtton.com>
2021-12-18 12:00:02 +0400

    catching args from the form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request,redirect
 
 app = Flask(__name__)
 
@@ -7,12 +7,17 @@ def index():
     if request.method == "POST":
         keywords = request.form["keywords"].split(",")
         websites = request.form["websites"].split(",")
-
+        print(keywords)
+        return redirect(url_for("loading",keywords=keywords,websites=websites))
     return render_template("starter.html")
 
 
-@app.route("/loading")
+@app.route("/loading", methods=["GET","POST"])
 def loading():
+    keywords = request.args
+    websites = request.args
+
+    print(request.args["keywords"])
     return render_template("spinner.html")
 
 if "__main__" == __name__:
