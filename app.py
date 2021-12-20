e86efbbdbe459aaefc8a375c2ed02bb096dd1ba7
Luka Lortkipanidze <l.lortkifanidze@noxtton.com>
2021-12-20 22:14:28 +0400

    getting keywords and websites from the form correctly

--- app.py
+++ app.py
@@ -5,19 +5,17 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        keywords = request.form["keywords"].split(",")
-        websites = request.form["websites"].split(",")
-        print(keywords)
+        keywords = request.form["keywords"]
+        websites = request.form["websites"]
         return redirect(url_for("loading",keywords=keywords,websites=websites))
     return render_template("starter.html")
 
 
 @app.route("/loading", methods=["GET","POST"])
 def loading():
-    keywords = request.args
-    websites = request.args
+    keywords = request.args["keywords"].split(',')
+    websites = request.args["websites"].split(',')
 
-    print(request.args["keywords"])
     return render_template("spinner.html")
 
 if "__main__" == __name__:
