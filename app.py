be28574390ddf3d5d733eb876871674dc22b9bd8
Luka Lortkipanidze <l.lortkifanidze@noxtton.com>
2021-12-21 21:28:30 +0400

    feat: added flask-restful

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask,render_template,url_for,request,redirect
+from flask_restful import Resource, Api,reqparse
 
 app = Flask(__name__)
+api = Api(app)
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('keywords')
+parser.add_argument('websites')
+
+
+
+
 
 @app.route("/",methods=["GET","POST"])
 def index():
@@ -13,10 +24,31 @@ def index():
 
 @app.route("/loading", methods=["GET","POST"])
 def loading():
-    keywords = request.args["keywords"].split(',')
-    websites = request.args["websites"].split(',')
+    keywords = request.args["keywords"]
+    websites = request.args["websites"]
 
-    return render_template("spinner.html")
+    return render_template("spinner.html",keywords = keywords,websites = websites)
+
+
+class Sentences(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+    def post(self):
+        args = parser.parse_args()
+
+        keywords = args["keywords"].split(',')
+        websites = args["websites"].split(',')
+
+        return {"fuck" : "YOU"}
+
+
+
+
+
+api.add_resource(Sentences, '/hello_world')
+
 
 if "__main__" == __name__:
     app.run()
